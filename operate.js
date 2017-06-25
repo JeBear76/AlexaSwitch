7ea1455751cf1ff30bc1aeeeff10d96ceda22697
JeBear76 <jerepondumie@gmail.com>
2017-06-25 16:20:21 +0200

    Added actual call to updateShiftRegister() :D

--- operate.js
+++ operate.js
@@ -13,6 +13,7 @@ exports.operate = function(switchposition){
     for (var index = 0; index < switches.length; index++) {
         switches[index] = ((Math.pow(2, index) & switchposition) === Math.pow(2, index));        
     }
+    updateShiftRegister();
 }
  
 function updateShiftRegister()
